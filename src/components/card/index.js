e5a55b0df51888d28882ea1f854624afdbf0cd1f
FERHATSUMER <sumerf39@gmail.com>
2023-05-05 14:03:57 +0300

    informationcard-react

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,13 +2,15 @@ import "./card.css";
 
 const Card = ({ name, img, job, about, id }) => {
   return (
-    <div key={id} className="container">
-      <div className="card">
-        <img src={img} alt="resim" className="photo" />
-        <div className="intro">
-          <h1>{name}</h1>
-          <h3>{job}</h3>
-          <h4>{about}</h4>
+    <div className="card-container">
+      <div key={id} className="container">
+        <div className="card">
+          <img src={img} alt="resim" className="photo" />
+          <div className="intro">
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <h4>{about}</h4>
+          </div>
         </div>
       </div>
     </div>
